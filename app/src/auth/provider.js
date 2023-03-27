48cadee88847c04cf97f44b4d04da2796ddef0ab
Michele Zamuner <michelezamuner@gmail.com>
2023-03-27 13:13:30 +0200

    Remove dead code

--- app/src/auth/provider.js
+++ app/src/auth/provider.js
@@ -1,4 +1,3 @@
-// import Gapi from '../../lib/gapi.js';
 import GapiDriver from './drivers/gapi_driver.js';
 import Service from './application/service.js';
 import Api from './clients/api.js';
@@ -9,7 +8,6 @@ export default class Provider {
   }
 
   provide(api) {
-    // const gapi = new Gapi(window, document, this._config.gapi);
     const gapiDriver = new GapiDriver(api);
 
     const service = new Service(gapiDriver);
